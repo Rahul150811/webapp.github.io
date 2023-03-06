ba28f4e79cfbe9dc2cf154db31cabacfb1cbb648
Rahul Fnu <frahul@schools.nyc.gov>
2023-03-06 14:28:11 -0500

    Initial Commit

--- flask.py
+++ flask.py
@@ -1,26 +1,10 @@
-from flask import Flask
-from pytube import YouTube
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return '''
-        <form method="post" action="/download">
-            <label for="url">Enter YouTube video URL:</label>
-            <input type="text" name="url" id="url">
-            <button type="submit">Download</button>
-        </form>
-    '''
-
-@app.route('/download', methods=['POST'])
-def download():
-    url = request.form.get('url')
-    video = YouTube(url)
-    stream = video.streams.get_highest_resolution()
-    filename = f'{video.title}.mp4'
-    stream.download(filename)
-    return send_file(filename, as_attachment=True)
+def home():
+    return send_from_directory('static', 'index.html')
 
 if __name__ == '__main__':
     app.run()
