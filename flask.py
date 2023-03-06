3c614ae79f2ddd16b20944908f0b6ef9adfbe4b9
Rahul Fnu <frahul@schools.nyc.gov>
2023-03-06 14:31:18 -0500

    Initial Commit

--- flask.py
+++ flask.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template, send_from_directory
+import os
+from flask import Flask, render_template, request, send_from_directory
+from pytube import YouTube
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return send_from_directory('static', 'index.html')
+    return render_template('index.html')
+
+@app.route('/download', methods=['POST'])
+def download():
+    url = request.form['url']
+    yt = YouTube(url)
+    ys = yt.streams.get_highest_resolution()
+    downloads_folder = os.path.expanduser('~/Downloads')
+    ys.download(downloads_folder)
+    return f'Successfully downloaded {ys.default_filename} to {downloads_folder}.'
 
 if __name__ == '__main__':
     app.run()
